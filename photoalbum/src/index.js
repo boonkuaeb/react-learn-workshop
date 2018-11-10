ba071e8e862ca32cd4db1f2f77abf95fa044e9cb
Boonkuae Boonsutta <boonkuae.boo@ascendcorp.com>
2018-11-10 23:25:09 +0700

    Basic redux configuration

--- photoalbum/src/index.js
+++ photoalbum/src/index.js
@@ -4,7 +4,25 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter as Router} from 'react-router-dom';
+import {createStore} from 'redux'
 
+function countAge(state =0, action)
+{
+    switch (action.type) {
+        case 'UP_AGE':
+            return state+1;
+        case 'DOWN_AGE':
+            return state-1;
+        default:
+            return state;
+    }
+}
+
+const store = createStore(countAge);
+store.subscribe(()=>console.log(store.getState()));
+store.dispatch({
+    type:'DOWN_AGE'
+});
 
 ReactDOM.render(
     <Router>
