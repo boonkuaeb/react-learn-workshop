907b32394b780564904b8448670a53b75ab5c809
Boonkuae Boonsutta <boonkuae.boo@ascendcorp.com>
2018-11-10 23:15:49 +0700

    State problem when Route URK changed

--- photoalbum/src/pages/Home.js
+++ photoalbum/src/pages/Home.js
@@ -1,10 +1,32 @@
 import React, {Component} from 'react';
 
 class Home extends Component {
+
+    state = {age: 0};
+
+    upAge = () => {
+        this.setState((prevState) => {
+            return {
+                age: prevState.age + 1
+            }
+        })
+    };
+
+    downAge = () => {
+        this.setState((prevState) => {
+            return {
+                age: prevState.age - 1
+            }
+        })
+    };
+
+
     render() {
         return (
             <div className="App-Home">
-                <h1>This is Home Component</h1>
+                <h1>Your age is  {this.state.age} years</h1>
+                <button onClick={this.upAge}>+ Age</button>
+                <button onClick={this.downAge}>- Age</button>
             </div>
         );
     }
