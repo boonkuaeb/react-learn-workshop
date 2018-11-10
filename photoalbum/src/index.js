b1617726aea24a821abc2f33a861ff91d0bd2c57
Boonkuae Boonsutta <boonkuae.boo@ascendcorp.com>
2018-11-10 23:48:17 +0700

    call state from component

--- photoalbum/src/index.js
+++ photoalbum/src/index.js
@@ -4,30 +4,33 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter as Router} from 'react-router-dom';
-import {createStore} from 'redux'
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 
-function countAge(state =0, action)
-{
+
+function countAge(state = 0, action) {
     switch (action.type) {
         case 'UP_AGE':
-            return state+1;
+            return state + 1;
         case 'DOWN_AGE':
-            return state-1;
+            return state - 1;
         default:
             return state;
     }
 }
 
 const store = createStore(countAge);
-store.subscribe(()=>console.log(store.getState()));
+store.subscribe(() => console.log(store.getState()));
 store.dispatch({
-    type:'DOWN_AGE'
+    type: ''
 });
 
 ReactDOM.render(
-    <Router>
-        <App />
-    </Router>
+    <Provider store={store}>
+        <Router>
+            <App/>
+        </Router>
+    </Provider>
     , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
--- photoalbum/src/pages/Home.js
+++ photoalbum/src/pages/Home.js
@@ -1,35 +1,37 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux'
 
 class Home extends Component {
-
-    state = {age: 0};
-
-    upAge = () => {
-        this.setState((prevState) => {
-            return {
-                age: prevState.age + 1
-            }
-        })
-    };
-
-    downAge = () => {
-        this.setState((prevState) => {
-            return {
-                age: prevState.age - 1
-            }
-        })
-    };
-
 
     render() {
         return (
             <div className="App-Home">
-                <h1>Your age is  {this.state.age} years</h1>
-                <button onClick={this.upAge}>+ Age</button>
-                <button onClick={this.downAge}>- Age</button>
+                <h1>Your age is {this.props.age} years</h1>
+                <button onClick={this.props.upAge}>+ Age</button>
+                <button onClick={this.props.downAge}>- Age</button>
             </div>
         );
     }
 }
+//********************************************************************
+//    aAll state will reset when refreshing the browser
+//********************************************************************
 
-export default Home;
+// Mapping Home component stage to Redux store as an props name age.
+function mapStateToProps(state) {
+    return {age: state}
+}
+
+// Define function to call a store command
+function mapDispatchToProps(dispatch) {
+    return {
+        downAge: () => {
+            dispatch({type: 'DOWN_AGE'})
+        },
+        upAge: () => {
+            dispatch({type: 'UP_AGE'})
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
