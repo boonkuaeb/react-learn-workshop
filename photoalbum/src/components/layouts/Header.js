1c23bb886d376beef53fa1000228aeb33a066050
Boonkuae Boonsutta <boonkuae.boo@ascendcorp.com>
2018-11-12 14:21:31 +0700

    Show/Hide Menu

--- photoalbum/src/components/layouts/Header.js
+++ photoalbum/src/components/layouts/Header.js
@@ -1,19 +1,37 @@
 import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
+import {Link, withRouter} from 'react-router-dom';
 
 class Header extends Component {
+
+    handleSignOut = (e) => {
+        e.preventDefault();
+        localStorage.removeItem('access-token');
+        this.props.history.push('/sign-in');
+    };
+
+    renderLink = () => {
+        if (localStorage.getItem('access-token')) {
+            return (
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/user">Photo</Link></li>
+                    <li>
+                        <a href="/sign-out" onClick={this.handleSignOut}>Sign Out</a>
+                    </li>
+                </ul>
+            );
+        }
+    };
+
     render() {
         return (
             <div className="App-Header">
                 <h1>Header</h1>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/user">Photo</Link></li>
-                </ul>
+                {this.renderLink()}
 
             </div>
         );
     }
 }
 
-export default Header;
+export default withRouter(Header);
